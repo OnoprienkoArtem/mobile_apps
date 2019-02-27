fb4093ecf435e553f1530d944e5dd90ae9d66b4e
OnoprienkoArtem <o.artem.e@gmail.com>
2019-02-27 23:18:41 +0200

    practice created mobile app using framework7-vue

---
+++ framework7-vue/src/pages/about.vue
@@ -0,0 +1,26 @@
+<template>
+  	<f7-page name="about">
+		  
+    <f7-navbar title="About" back-link="Back"></f7-navbar>
+
+    <f7-block-title>About My App</f7-block-title>
+
+		<f7-block strong>
+			<p>Fugiat perspiciatis excepturi, soluta quod non ullam deleniti. Nobis sint nemo consequuntur, fugiat. Eius perferendis animi autem incidunt vel quod tenetur nostrum, voluptate omnis quasi quidem illum consequuntur, a, quisquam.</p>
+			<p>Laudantium neque magnam vitae nemo quam commodi, in cum dolore obcaecati laborum, excepturi harum, optio qui, consequuntur? Obcaecati dolor sequi nesciunt culpa quia perspiciatis, reiciendis ex debitis, ut tenetur alias.</p>
+		</f7-block>
+
+		<f7-block>
+			<f7-card 
+				title="Card header" 
+				content="Card with header and footer. Card headers are used to display card titles and footers for additional information or just for custom actions." 
+				footer="Card footer"
+		></f7-card>
+<f7-card
+  content="Another card. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse feugiat sem est, non tincidunt ligula volutpat sit amet. Mauris aliquet magna justo. "
+></f7-card>
+		</f7-block>
+
+
+  	</f7-page>
+</template>
